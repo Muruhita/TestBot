19b827d746311547ea4401773c66323e1100e2ff
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-04 06:23:14 +0700

    Update submit.js

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -36,8 +36,8 @@ const webhooks = {
   reinstatement: process.env.WEBHOOK_REINSTATEMENT,
   transferToFib: process.env.WEBHOOK_TRANSFER_TO_FIB,
   weaponRequest: process.env.WEBHOOK_WEAPON_REQUEST,
+  withdrawal: process.env.WEBHOOK_WITHDRAWAL,
   leave: process.env.WEBHOOK_LEAVE,
-  withdrawal: process.env.WEBHOOK_WITHDRAWAL
 };
 };
 
